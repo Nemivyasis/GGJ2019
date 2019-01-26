33f81d498457bafafcae6fb0fd4eec7677170a40
Guest User <Guest User@DESKTOP-3FUF4BC>
2019-01-26 15:27:10 -0500

    Following mouse script

---
+++ Homing Cow/Assets/Script/followMouseScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(Mathf.Floor(mousePos.x + .5f), Mathf.Floor(mousePos.y + .5f), -5);
+	}
+}

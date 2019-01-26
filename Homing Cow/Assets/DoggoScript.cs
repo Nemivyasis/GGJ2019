200f4aba3a42f27c3b317a27dbb487cb848efd72
Vincent-DiCocco <vincent.dicocco@hotmail.com>
2019-01-25 21:42:12 -0500

    Basic doggo movement

---
+++ Homing Cow/Assets/DoggoScript.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoggoScript : MonoBehaviour {
+
+    public enum directions
+    {
+        right,
+        left,
+        down,
+        up
+    }
+    public enum AI
+    {
+        forwardRight
+    }
+
+    public float updateTime = .5f;
+    float sinceLastUpdate;
+
+    AI aiChoice;
+    public directions startingForward = directions.right;
+    directions forward;
+
+	// Use this for initialization
+	void Start () {
+        sinceLastUpdate = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sinceLastUpdate += Time.deltaTime;
+
+        if(sinceLastUpdate >= updateTime)
+        {
+            sinceLastUpdate = 0;
+
+            switch (aiChoice)
+            {
+                case AI.forwardRight:
+                    ForwardRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+	}
+
+    private void ForwardRight()
+    {
+        switch (forward)
+        {
+            case directions.right:
+                transform.Translate(new Vector3(1, 0, 0));
+                break;
+            case directions.left:
+                transform.Translate(new Vector3(-1, 0, 0));
+                break;
+            case directions.down:
+                transform.Translate(new Vector3(0, -1, 0));
+                break;
+            case directions.up:
+                transform.Translate(new Vector3(0, 1, 0));
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Homing Cow/Assets/GridManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+
+    bool[,] open;
+    public int maxX=10;
+    public int maxY = 10;
+    public Vector2 leftCorner = new Vector2(5.5f, -5.5f);
+	// Use this for initialization
+	void Start () {
+        open = new bool[100,100];
+ 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

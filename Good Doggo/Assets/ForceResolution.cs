a6b7a86f5b557f05f9fa50d85cde3b5e19b37f2f
Guest User <Guest User@DESKTOP-3FUF4BC>
2019-01-27 14:56:32 -0500

    Renamed and music and forcing resolution

---
+++ Good Doggo/Assets/ForceResolution.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceResolution : MonoBehaviour {
+
+    private void Awake()
+    {
+        Screen.SetResolution(1024, 768, false);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

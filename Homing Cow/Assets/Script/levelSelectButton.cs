e04f6e9a561afb8cfb1f91c22f0e47919eee3fad
Guest User <Guest User@DESKTOP-3FUF4BC>
2019-01-27 14:09:58 -0500

    Level Select

---
+++ Homing Cow/Assets/Script/levelSelectButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelSelectButton : MonoBehaviour {
+
+    public int levelNum;
+    int levelBuildOffset = 1;
+
+    public void levelSelect()
+    {
+        SceneManager.LoadScene(levelNum + levelBuildOffset - 1);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
